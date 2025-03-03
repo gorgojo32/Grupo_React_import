b2f5f3d27588ac41823cfbd5d89f1009105c7887
OtroDevMas <juliancarrascal365@gmail.com>
2025-03-03 13:51:45 -0500

    Pendejadas

--- vite/src/pages/usuarios.tsx
+++ vite/src/pages/usuarios.tsx
@@ -1,6 +1,27 @@
-import * as React from 'react';
-import Typography from '@mui/material/Typography';
-
 export default function Usuarios() {
-    return <Typography>Hola Usuario</Typography>;
+    return (
+
+        <div className="col-9">
+            <div className="card" style={{ width: '18rem', margin: '20px', padding: '20px' }}>
+                <img
+                    src="/licAguilar.jpg"
+                    className="card-img-top"
+                    alt="Foto de perfil"
+                />
+                <div className="card-body">
+                    <h5 className="card-title">Lic. Aguilar</h5>
+                    <p className="card-text">
+                        <strong>Edad:</strong> 129 años <br />
+                        <strong>Ocupación:</strong> Desarrollador Web y Abogado<br />
+                        <strong>Email:</strong> licenciadoAguilar@example.com <br />
+                        <strong>Dirección:</strong> Calle Falsa 123, Ciudad Imaginaria
+                    </p>
+                    <a href="mailto:juan.perez@example.com" className="btn btn-primary">
+                        Contactar
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    );
 }
